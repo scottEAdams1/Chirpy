6f1a4b1e0752168129a7cc7a4060c881392d9002
scottEAdams1 <scott.e.adams01@gmail.com>
2024-07-15 21:05:47 +0100

    Added html and an image

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 
-	srv := &http.Server{
+	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
-	log.Fatal(srv.ListenAndServe())
+	log.Fatal(server.ListenAndServe())
+
 }
